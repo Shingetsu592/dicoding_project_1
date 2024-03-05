ad9f8abcf8c3db53ba95931ffa9265bb74a903b9
Shingetsu592 <czheng592@gmail.com>
2024-03-05 21:07:02 +0700

    update new questions in notebook and add new plot to dashboard

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -2,7 +2,6 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import os
 
 names = []
 month_name = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
@@ -23,11 +22,9 @@ def data_date(df):
     df['date'] = pd.to_datetime(df[['year', 'month', 'day']])
     return df
 
-def plot_CO(df, parameter):
-    # plot CO distribution for each station towards x with year and month parameter in different plots and selectbox
-    st.write('CO distribution for each station')
-    station = df['station'].unique()
-    station_select = st.selectbox('Select Station', station)
+def station_and_date_select(df):
+    st.write('CO, PM 2.5, and PM 10 distribution for each station')
+    station = st.selectbox('Select Station', df['station'].unique())
     st.write('Year')
     year = st.slider('Select Year', 2013, 2017)
     st.write('Month')
@@ -37,11 +34,15 @@ def plot_CO(df, parameter):
         month = st.slider('Select Month', 1, 2)
     else:
         month = st.slider('Select Month', 1, 12)
-    df_selected = df[(df['station'] == station_select) & (df['year'] == year) & (df['month'] == month)]
-    plt.bar(df_selected['date'], df_selected[parameter])
+    df_selected = df[(df['station'] == station) & (df['year'] == year) & (df['month'] == month)]
+    return df_selected, station, year, month
+
+def plot_distribution(df, parameter, station, year, month):
+    # plot CO or PM2.5 distribution for each station towards x with year and month parameter in different plots and selectbox
+    plt.bar(df['date'], df[parameter])
     plt.xlabel('Day')
-    plt.ylabel('CO')
-    plt.title('CO distribution for station {} in {} {}'.format(station_select, month_name[month-1], year))
+    plt.ylabel(parameter)
+    plt.title('{} distribution for station {} in {} {}'.format(parameter, station, month_name[month-1], year))
     plt.xticks(rotation=90)  # This will skew the x values
     plt.tight_layout()  # This will fit the plot to the available space
     st.pyplot(plt)
@@ -52,4 +53,7 @@ df = pd.read_csv(data_path)
 df = clean_data(df)
 df = data_date(df)
 st.dataframe(df, use_container_width=True)
-plot_CO(df, parameter='CO')
+df_selected, station, year, month = station_and_date_select(df)
+plot_distribution(df_selected, parameter='CO', station=station, year=year, month=month)
+plot_distribution(df_selected, parameter='PM2.5', station=station, year=year, month=month)
+plot_distribution(df_selected, parameter='PM10', station=station, year=year, month=month)
